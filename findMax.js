b83242093afa50b194a2fdcbd8895a8b13754375
Kyler Kent <37264045+mazda4409@users.noreply.github.com>
2021-07-05 17:34:25 -0500

    Create findMax.js

---
+++ findMax.js
@@ -0,0 +1,13 @@
+var team1 =["2","4","6","8",10];
+var team2 =[];
+
+for (var i = 0; i < team1.length; i++) {
+    if ((i>i++)==true){
+	team2.push(team1[i]);}
+    if ((i<i++)==true){
+     team2.push(team1[i++]);}
+}
+
+console.log(team2)
+	
+	
